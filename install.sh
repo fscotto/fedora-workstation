d464faa91ec664d7b31c0ea53ae30373d1b274bb
Fabio Scotto di Santolo <f.scottodisantolo@techgap.it>
2024-07-03 11:20:28 +0200

    Fix installation script for Fedora Workstation

--- install.sh
+++ install.sh
@@ -6,25 +6,26 @@ MY_HOME="$HOME"
 echo -e "Script directory is $SCRIPT_DIR\n"
 echo -e "Home directory is $MY_HOME\n"
 
-rpm-ostree cancel
+sudo dnf upgrade --assumeyes
 
 echo -e "Add RPM Fusion (free and non-free) repositories\n"
-sudo rpm-ostree install --assumeyes --idempotent\
-    https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm\
-    https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
-sudo rpm-ostree apply-live
+echo -e "Add third party repositories from Copr\n"
+sudo dnf copr enable --assumeyes varlad/zellij
+sudo dnf copr enable --assumeyes atim/starship
+sudo dnf copr enable --assumeyes coder966/postman
+sudo dnf copr enable --assumeyes coder966/intellij-idea-ultimate
 
-echo -e "Install layered packages\n"
-rpm-ostree install -A --assumeyes --idempotent "$(cat "$SCRIPT_DIR/data/silverblue-pkg.txt")"
+# Add official repository for VSCodium
+sudo rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
+printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h" | sudo tee -a /etc/yum.repos.d/vscodium.repo
+
+echo -e "Install Fedora packages\n"
+sudo dnf install --assumeyes "$(cat "$SCRIPT_DIR/data/fedora-pkg.txt")"
 
 echo -e "Remove Firefox from image\n"
-rpm-ostree override remove firefox-langpacks firefox
-
-echo -e "Remove GNOME Terminal from image\n"
-rpm-ostree override remove gnome-terminal-nautilus gnome-terminal
-
-sudo rpm-ostree apply-live
+sudo dnf remove --assumeyes firefox-langpacks firefox
 
 echo -e "Add Flathub repository\n"
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
@@ -36,30 +37,20 @@ done < "$SCRIPT_DIR/data/flatpak.txt"
 
 mkdir -p ~/.local/bin/
 
-# TODO configure and export apps and binaries from Distrobox's container
-echo -e "Create Archlinux container\n"
-distrobox create -n arch -i quay.io/toolbox/arch-toolbox:latest -H "$HOME/.local/share/distrobox/home/arch" --no-entry -Y
-distrbox-enter arch -- yes | sudo pacman -Syu
-distrobox-enter arch -- git clone https://aur.archlinux.org/paru.it &&\
-    cd paru &&\
-    makepkg -si
-distrobox-enter arch -- yes | paru -S lazygit lazydocker k9s kubectl
-distrobox-enter arch -- distrobox-export --bin /usr/bin/lazygit --export-path "$MY_HOME/.local/bin"
-distrobox-enter arch -- distrobox-export --bin /usr/bin/lazydocker --export-path "$MY_HOME/.local/bin"
-distrobox-enter arch -- distrobox-export --bin /usr/bin/k9s --export-path "$MY_HOME/.local/bin"
-distrobox-enter arch -- distrobox-export --bin /usr/bin/kubectl --export-path "$MY_HOME/.local/bin"
+# Copy dotfiles
+configs=("bat" "fastfetch" "fish" "nvim" "ranger" "starship.toml")
+git clone https://github.com/fscotto/dotfiles.git
+for f in "${configs[@]}"; do 
+    rsync -avxHAWX --delete --fsync "$SCRIPT_DIR/dotfiles/$f" "$HOME/.config"    
+done
 
-echo -e "Create Fedora 40 container\n"
-distrobox create -n fedora40 -i registry.fedoraproject.org/fedora-toolbox:40 --no-entry -Y
-distrobox-enter fedora40 -- sudo dnf upgrade --assumeyes
-distrobox-enter fedora40 -- sudo dnf copr enable atim/starship --assumeyes && sudo dnf install starship --assumeyes
-distrobox-enter fedora40 -- distrobox-export --bin /usr/bin/starship --export-path "$MY_HOME/.local/bin"
-
-# TODO Copy dotfiles
+dotfiles=(".gitignore_global" ".gitconfig")
+for f in "${dotfiles[@]}"; do
+    rsync -avxHAWX --delete --fsync "$SCRIPT_DIR/dotfiles/$f" "$HOME" 
+done
 
 # TODO install all GNOME extensions
 
 # TODO Restore DConf settings
 
-rpm-ostree update
-#sudo systemctl reboot
+echo -e "Apply all setting reboot your computer"
