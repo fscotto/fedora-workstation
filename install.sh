ee80d7d43ff0e6502ccdbc4b404a05b6e5d267cf
Fabio Scotto di Santolo <f.scottodisantolo@techgap.it>
2024-07-03 11:44:28 +0200

    Fix installation RPM packages

--- install.sh
+++ install.sh
@@ -1,15 +1,53 @@
 #!/usr/bin/env bash
 
+declare -a PACKAGES=(
+    "bat"
+    "btop"
+    "codium"
+    "dconf-editor"
+    "duf"
+    "elixir"
+    "erlang"
+    "eza"
+    "fastfetch"
+    "fastfetch-fish-completion"
+    "fd-find"
+    "filezilla"
+    "firewall-config"
+    "fish"
+    "fzf"
+    "htop"
+    "httpie"
+    "hugo"
+    "hwinfo"
+    "intel-media-driver"
+    "intellij-idea-ultimate"
+    "jq"
+    "libavcodec-freeworld"
+    "neovim"
+    "meld"
+    "openssl"
+    "pipewire-codec-aptx"
+    "postman"
+    "python3-virtualenv"
+    "qemu"
+    "ranger"
+    "rsync"
+    "rustup"
+    "starship"
+    "ugrep"
+    "xclip"
+    "zellij"
+)
+
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-MY_HOME="$HOME"
 
 echo -e "Script directory is $SCRIPT_DIR\n"
-echo -e "Home directory is $MY_HOME\n"
 
 sudo dnf upgrade --assumeyes
 
 echo -e "Add RPM Fusion (free and non-free) repositories\n"
-sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install --assumeyes https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 echo -e "Add third party repositories from Copr\n"
 sudo dnf copr enable --assumeyes varlad/zellij
@@ -21,8 +59,8 @@ sudo dnf copr enable --assumeyes coder966/intellij-idea-ultimate
 sudo rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
 printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h" | sudo tee -a /etc/yum.repos.d/vscodium.repo
 
-echo -e "Install Fedora packages\n"
-sudo dnf install --assumeyes "$(cat "$SCRIPT_DIR/data/fedora-pkg.txt")"
+echo -e "Install RPM packages\n"
+sudo dnf install --assumeyes "${PACKAGES[@]}"
 
 echo -e "Remove Firefox from image\n"
 sudo dnf remove --assumeyes firefox-langpacks firefox
@@ -48,6 +86,8 @@ dotfiles=(".gitignore_global" ".gitconfig")
 for f in "${dotfiles[@]}"; do
     rsync -avxHAWX --delete --fsync "$SCRIPT_DIR/dotfiles/$f" "$HOME" 
 done
+
+rm -rf "$SCRIPT_DIR/dotfiles"
 
 # TODO install all GNOME extensions
 
