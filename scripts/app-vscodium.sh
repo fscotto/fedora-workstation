d4de7362f4bfb4e09b8c8cdc5f0f72ba1b83f4ce
Fabio Scotto di Santolo <f.scottodisantolo@techgap.it>
2024-07-04 14:44:00 +0200

    Install VSCodium extensions and configure editor

--- scripts/app-vscodium.sh
+++ scripts/app-vscodium.sh
@@ -6,3 +6,49 @@ printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbase
 
 echo -e "\nInstall VSCodium\n"
 sudo dnf install --assumeyes codium
+
+echo -e "Install VSCodium Extensions\n"
+
+codium --install-extension piousdeer.adwaita-theme
+codium --install-extension redhat.ansible
+codium --install-extension formulahendry.auto-close-tag
+codium --install-extension bungcip.better-toml
+codium --install-extension franneck94.vscode-rust-extension-pack
+codium --install-extension mikestead.dotenv
+codium --install-extension elixir-lsp.elixir-ls
+codium --install-extension golang.go
+codium --install-extension rusnasonov.vscode-hugo
+codium --install-extension budparr.language-hugo-vscode
+codium --install-extension DavidAnson.vscode-markdownlint
+codium --install-extension christian-kohler.path-intellisense
+codium --install-extension esbenp.prettier-vscode
+codium --install-extension ms-python.python
+codium --install-extension ms-python.debugpy
+codium --install-extension timonwong.shellcheck
+codium --install-extension redhat.vscode-xml
+codium --install-extension redhat.vscode-yaml
+
+
+cat << EOF > ~/.config/VSCodium/User/settings.json
+{
+    "workbench.startupEditor": "none",
+    "workbench.iconTheme": "material-icon-theme",
+    "workbench.colorTheme": "Adwaita Dark",
+    "window.titleBarStyle": "custom",
+
+    "editor.fontFamily": "'JetBrainsMono Nerd Font', 'FiraCode Nerd Font'",
+    "editor.fontSize": 18,
+    
+    // Terminal configurations
+    "terminal.external.linuxExec": "gnome-terminal",
+    "terminal.integrated.gpuAcceleration": "on",
+    "terminal.integrated.defaultProfile.linux": "fish",
+    "terminal.integrated.cursorBlinking": true,
+    "terminal.integrated.fontFamily": "'RobotoMono Nerd Font'",
+    "terminal.integrated.fontSize": 18,
+
+    "redhat.telemetry.enabled": false,
+    "elixirLS.enableTestLenses": true,
+    "elixirLS.fetchDeps": true,
+}
+EOF
