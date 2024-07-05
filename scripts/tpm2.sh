10bcb075d11e6854c2d89a8db625ed74eeae36fd
Fabio Scotto di Santolo <fabio.scottodisantolo@gmail.com>
2024-07-05 22:12:21 +0200

    Update tpm2.sh

--- scripts/tpm2.sh
+++ scripts/tpm2.sh
@@ -11,11 +11,11 @@ function configure_tpm_module
     echo "add_dracutmodules+=\" tpm2-tss \"" | sudo tee /etc/dracut.conf.d/tpm2.conf
     echo "add_dracutmodules+=\" bluetooth \"" | sudo tee /etc/dracut.conf.d/bluetooth.conf
 
-    sudo systemd-cryptenroll --wipe-slot tpm2 --tpm2-device auto --tpm2-pcrs "0+1+2+3+4+5+7+9" "$luks2_device"
+    sudo systemd-cryptenroll --wipe-slot tpm2 --tpm2-device auto --tpm2-pcrs "7" "$luks2_device"
 
     # Update /etc/crypttab and append tpm2-device=auto,tpm2-pcrs=0+1+2+3+4+5+7+9 to the appropriate line’s options, depending on the PCRs used.
     sudo cp /etc/crypttab /etc/crypttab.bak
-    sudo sed -e 's/$/ tpm2-device=auto,tpm2-pcrs=0+1+2+3+4+5+7+9/' -i crypttab
+    sudo sed -e 's/$/ tpm2-device=auto,tpm2-pcrs=0+1+2+3+4+5+7+9/' -i /etc/crypttab
 
     sudo dracut -f
 }
