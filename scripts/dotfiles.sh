3b40ba053bc6f793f9051410ce1441c7543c7cf3
Fabio Scotto di Santolo <fabio.scottodisantolo@gmail.com>
2024-07-24 08:54:04 +0200

    Replace Alacritty with Kitty

--- scripts/dotfiles.sh
+++ scripts/dotfiles.sh
@@ -9,16 +9,16 @@ echo -e "Copying dotfiles from my GitHub repo\n"
 git clone --recursive https://github.com/fscotto/dotfiles.git "$DOTFILES_LOC"
 
 packages=(
-    "alacritty"
-    "bat" 
-    "fastfetch" 
-    "fish" 
-    "git"
-    "nvim" 
-    "ranger"
-    "starship"
-    "tmux"
+  "bat"
+  "fastfetch"
+  "fish"
+  "git"
+  "kitty"
+  "nvim"
+  "starship"
+  "tmux"
+  "yazi"
 )
-for pkg in "${packages[@]}"; do 
-    stow --dotfiles --dir "$DOTFILES_LOC" "$pkg"
+for pkg in "${packages[@]}"; do
+  stow --dotfiles --dir "$DOTFILES_LOC" "$pkg"
 done
