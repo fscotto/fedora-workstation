e3b211b6b0678f6e23b7912bcd831903fb12646e
Fabio Scotto di Santolo <fabio.scottodisantolo@gmail.com>
2024-07-24 10:15:42 +0200

    Added dependency app for Yazi config

--- scripts/app-homebrew.sh
+++ scripts/app-homebrew.sh
@@ -3,4 +3,4 @@
 test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
 test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
-brew install kubernetes-cli openshift-cli argocd helm kind glab yazi lazygit lazydocker
+brew install kubernetes-cli openshift-cli argocd helm kind glab yazi lazygit lazydocker exiftool glow
