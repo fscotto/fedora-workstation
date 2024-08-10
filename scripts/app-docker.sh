3ba999160b1ef9463cd0c86bea0e3b07333d7659
Fabio Scotto di Santolo <fabio.scottodisantolo@gmail.com>
2024-08-10 16:48:13 +0200

    Fix DNF flag

--- scripts/app-docker.sh
+++ scripts/app-docker.sh
@@ -3,7 +3,7 @@
 echo -e "\n\nInstall Docker\n"
 sudo dnf install --assumeyes dnf-plugins-core
 sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
-sudo dnf install --assuemeyes docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo dnf install --assumeyes docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 echo -e "Configure user ${USER} with docker group\n"
 sudo usermod -aG docker "${USER}"
