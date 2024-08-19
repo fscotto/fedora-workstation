b3f5af78a27b980f2da2f057eb2b664b045d5b42
Fabio Scotto di Santolo <fabio.scottodisantolo@gmail.com>
2024-08-19 10:23:13 +0200

    Added Status Area Horizontal Spacing GNOME extension

--- scripts/set-gnome-extensions.sh
+++ scripts/set-gnome-extensions.sh
@@ -15,9 +15,10 @@ extensions=(
   "bluetooth-quick-connect@bjarosze.gmail.com"
   "user-theme@gnome-shell-extensions.gcampax.github.com"
   "no-overview@fthx"
+  "status-area-horizontal-spacing@mathematical.coffee.gmail.com"
 )
 for ext in "${extensions[@]}"; do
-  gext install --filesystem "$ext"
+  gext install "$ext"
 done
 
 sudo cp "${USER_EXT}"/bluetooth-quick-connect@bjarosze.gmail.com/schemas/org.gnome.shell.extensions.bluetooth-quick-connect.gschema.xml "${SYSTEM_EXT}"
@@ -25,11 +26,12 @@ sudo cp "${USER_EXT}"/caffeine@patapon.info/schemas/org.gnome.shell.extensions.c
 sudo cp "${USER_EXT}"/clipboard-history@alexsaveau.dev/schemas/org.gnome.shell.extensions.clipboard-indicator.gschema.xml "${SYSTEM_EXT}"
 sudo cp "${USER_EXT}"/gnome-ui-tune@itstime.tech/schemas/org.gnome.shell.extensions.gnome-ui-tune.gschema.xml "${SYSTEM_EXT}"
 sudo cp "${USER_EXT}"/user-theme@gnome-shell-extensions.gcampax.github.com/schemas/org.gnome.shell.extensions.user-theme.gschema.xml "${SYSTEM_EXT}"
+sudo cp "${USER_EXT}"/status-area-horizontal-spacing@mathematical.coffee.gmail.com/schemas/org.gnome.shell.extensions.status-area-horizontal-spacing.gschema.xml "${SYSTEM_EXT}"
 sudo glib-compile-schemas "${SYSTEM_EXT}"
 
 echo -e "Configure GNOME Extensions\n"
 
-# Background log
+# Background Logo
 gsettings set org.fedorahosted.background-logo-extension logo-always-visible true
 
 # Caffeine
@@ -37,3 +39,6 @@ gsettings set org.gnome.shell.extensions.caffeine inhibit-apps "['org.videolan.V
 
 # Bluetooth Quick Connect
 gsettings set org.gnome.shell.extensions.bluetooth-quick-connect show-battery-value-on true
+
+# Status Area Horizontal Spacing
+gsettings set org.gnome.shell.extensions.status-area-horizontal-spacing hpadding 5
