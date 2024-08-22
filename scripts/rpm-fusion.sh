0ed3e98a15b8b9dcbf5a881fe19fb75519601856
Fabio Scotto di Santolo <fabio.scottodisantolo@gmail.com>
2024-08-22 14:37:10 +0200

    Move from system to RPM Fusion packages

--- scripts/rpm-fusion.sh
+++ scripts/rpm-fusion.sh
@@ -6,8 +6,7 @@ if [ "$status" -ne 0 ]; then
   sudo dnf install --assumeyes https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
   echo -e "Install multimedia codecs\n"
-  sudo dnf install --assumeyes intel-media-driver libavcodec-freeworld pipewire-codec-aptx
+  sudo dnf install --assumeyes intel-media-driver libavcodec-freeworld pipewire-codec-aptx ffmpegthumbnailer telegram-desktop
 else
   echo -e "\nRPM Fusion repositories already installed\n"
 fi
-
