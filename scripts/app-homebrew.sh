820473825c2577b3a963b45a78d3a7435a31391b
Fabio Scotto di Santolo <fabio.scottodisantolo@gmail.com>
2024-08-23 10:05:04 +0200

    Fix Homebrew

--- scripts/app-homebrew.sh
+++ scripts/app-homebrew.sh
@@ -3,14 +3,15 @@
 if [ ! -d /home/linuxbrew/.linuxbrew ]; then
   echo -e "\n\nInstall Homebrew on Linux"
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-  test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
-  test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 else
   echo -e "\nHomebrew already installed"
 fi
 
 echo -e "Install Homebrew packages"
+
+test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
+test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
 brew install kubernetes-cli openshift-cli argocd helm kind glab yazi lazygit lazydocker exiftool glow viu moar
 brew install erlang_ls
 echo -e "Homebrew packages installed"
