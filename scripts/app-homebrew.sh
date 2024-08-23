760ff29c1c97fd8c43d4faa716fa5a6c24ed0626
Fabio Scotto di Santolo <fabio.scottodisantolo@gmail.com>
2024-08-23 11:26:04 +0200

    Fix Homebrew

--- scripts/app-homebrew.sh
+++ scripts/app-homebrew.sh
@@ -8,10 +8,10 @@ else
 fi
 
 echo -e "Install Homebrew packages"
-
 test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
 test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
-brew install kubernetes-cli openshift-cli argocd helm kind glab yazi lazygit lazydocker exiftool glow viu moar
+brew install kubernetes-cli openshift-cli argocd helm kind glab yazi
+brew install lazygit lazydocker exiftool glow viu moar
 brew install erlang_ls
 echo -e "Homebrew packages installed"
